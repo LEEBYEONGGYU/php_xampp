15f9bb977cacea69a110dbfd5b6cf3bc02c30431
LEEBYEONGGYU <55679098+LEEBYEONGGYU@users.noreply.github.com>
2022-08-09 21:59:27 +0900

    0809
    0809

--- xampp/htdocs/include/header.php
+++ xampp/htdocs/include/header.php
@@ -3,7 +3,7 @@
 <head>
     <title></title>
 <link rel="stylesheet" href="/css/common.css" />
-<script></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 <header>
@@ -20,15 +20,57 @@
         <div id="catagory_title">카테고리</div>
         <nav id="menu">
             <ul>
-                <li>가전, TV</li>
-                <li>컴퓨터 노트북, 조립PC</li>
-                <li>모바일</li>
-                <li>카메라</li>
-                <li>자전거, 스포츠</li>
+                <li class="menu_t1">가전, TV</li>
+                <li class="menu_t2">컴퓨터 노트북, 조립PC</li>
+                <li class="menu_t3">모바일</li>
+                <li class="menu_t4">카메라</li>
+                <li class="menu_t5">자전거, 스포츠</li>
             </ul>
         </nav>
     </div>
     <div id="catagory_hover_img">
-        <img src="/upload/admin/catagory/cat1_img.png" />
+        <img src="/upload/admin/catagory/cat1_img.png" id="catagory_img1" />
+        <img src="/upload/admin/catagory/cat1_2img.png" id="catagory_img2" class="def_none"/>
+        <img src="/upload/admin/catagory/cat1_3img.png" id="catagory_img3" class="def_none"/>
+        <img src="/upload/admin/catagory/cat1_4img.png" id="catagory_img4" class="def_none"/>
+        <img src="/upload/admin/catagory/cat1_5img.png" id="catagory_img5" class="def_none"/>
     </div>
 </header>
+
+<script>
+    $(".menu_t1").hover(function(){
+        //mouseover
+        $("#catagory_img1").show();
+    },function(){
+        //mouseout
+        $("#catagory_img1").hide();
+    });
+
+    $(".menu_t2").hover(function(){
+        $("#catagory_img2").show();
+    },function(){
+        $("#catagory_img2").hide();
+    });
+
+    $(".menu_t3").hover(function(){
+        $("#catagory_img3").show();
+    },function(){
+        $("#catagory_img3").hide();
+    });
+
+    $(".menu_t4").hover(function(){
+        //mouseover
+        $("#catagory_img4").show();
+    },function(){
+        //mouseout
+        $("#catagory_img4").hide();
+    });
+    $(".menu_t5").hover(function(){
+        //mouseover
+        $("#catagory_img5").show();
+    },function(){
+        //mouseout
+        $("#catagory_img5").hide();
+    });
+
+</script>
