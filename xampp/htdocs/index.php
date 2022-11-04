84b779b24ea74024bb5c6f7d0b4f095f57ef0c8e
LEEBYEONGGYU <55679098+LEEBYEONGGYU@users.noreply.github.com>
2022-11-04 17:51:01 +0900

    1104
    1104

--- xampp/htdocs/index.php
+++ xampp/htdocs/index.php
@@ -1,95 +1,175 @@
-<?php include $_SERVER['DOCUMENT_ROOT']."/include/header.php"; ?>
+<?php 
+  include $_SERVER['DOCUMENT_ROOT']."/db.php";
+?>
+<!doctype html>
+<head>
+<meta charset="UTF-8">
+<title>게시판</title>
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+</head>
+<body>
+<div id="board_area"> 
+  <h1>자유게시판</h1>
+  <h4>자유롭게 글을 쓸 수 있는 게시판입니다.</h4>
+    <table class="list-table">
+      <thead>
+          <tr>
+            <?php 
+              if(isset($_GET['sortType'])){
+                $sortType = $_GET['sortType'];
+              }else{
+                $sortType = 'desc';
+              }
+            
+            ?>
+              <th width="70"><a onclick="b_sort('idx','<?php echo $sortType ?>')" href="#">번호</th>
+                <th width="500"><a onclick="b_sort('title','<?php echo $sortType ?>')" href="#">제목</th>
+                <th width="120"><a onclick="b_sort('writer','<?php echo $sortType ?>')" href="#">글쓴이</th>
+                <th width="100"><a onclick="b_sort('date','<?php echo $sortType ?>')" href="#">작성일</th>
+                <th width="100"><a onclick="b_sort('hit','<?php echo $sortType ?>')" href="#">조회수</th>
+            </tr>
+        </thead>
+        <?php echo $sortColumn = $_GET['cloumnName'];
+         echo $sortType = $_GET['sortType']; ?>
+        <?php
+        
+            if(isset($_GET['page'])){
+              $page = $_GET['page'];
+                  $sortColumn = $_GET['cloumnName'];
+                  $sortType = $_GET['sortType'];
+                }else{
+                  $page = 1;
+                  $sortColumn = 'idx';
+                  $sortType = 'desc';
+                }
+                  
 
-<section>
+                  $sql = mq("select * from board");
+                  $row_num = mysqli_num_rows($sql); //게시판 총 레코드 수
+                  $list = 5; //한 페이지에 보여줄 개수
+                  $block_ct = 5; //블록당 보여줄 페이지 개수
 
+                  $block_num = ceil($page/$block_ct); // 현재 페이지 블록 구하기
+                  $block_start = (($block_num - 1) * $block_ct) + 1; // 블록의 시작번호
+                  $block_end = $block_start + $block_ct - 1; //블록 마지막 번호
 
+                  $total_page = ceil($row_num / $list); // 페이징한 페이지 수 구하기
+                  if($block_end > $total_page) $block_end = $total_page; //만약 블록의 마지박 번호가 페이지수보다 많다면 마지박번호는 페이지 수
+                  $total_block = ceil($total_page/$block_ct); //블럭 총 개수
+                  $start_num = ($page-1) * $list; //시작번호 (page-1)에서 $list를 곱한다.
 
-<section id="main_banner">
-	<div id="banner">
-		<img src="" alt="" title="" />
-		<img src="" alt="" title="" />
-		<img src="" alt="" title="" />
-	</div>
-</section>
+                  $sql2 = mq("select * from board order by $sortColumn $sortType limit $start_num, $list");  
+                  while($board = $sql2->fetch_array()){
+                  $title=$board["title"]; 
+                    if(strlen($title)>30)
+                    { 
+                      $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+                    }
+                    $sql3 = mq("select * from reply where con_num='".$board['idx']."'");
+                    $rep_count = mysqli_num_rows($sql3);
+                  ?>
+      <tbody>
+        <tr>
+          <td width="70"><?php echo $board['idx']; ?></td>
+          <td width="500">
+            <?php 
+              $lockimg = "<img src='/img/lock.png' alt='lock' title='lock' with='20' height='20' />";
+              if($board['lock_post']=="1")
+              { ?><a href='/page/board/ck_read.php?idx=<?php echo $board["idx"];?>'><?php echo $title, $lockimg;
+              }else{?>
 
-<section id="main_sec1">
-	<div id="main_sec1_in">
-		<div id="sec1_title">현재 인기있는 상품 </div>
-			<div id="sec1_product">
-				<div class="product_pulinfo">
-					<a href="/page/browsing.php"><div class="product_pulimg"><img src="/upload/admin/product/ssd.jpg" width="370" height="320" /></div></a>
-					<div class="product_pultitle">상품1</div>
-					<div class="product_pulprice">23,800원</div>
-					<div class="product_pulbuycount">10,741개 구매</div>
-				</div>
-
-				<div class="product_pulinfo">
-					<div class="product_pulimg"><img src="/upload/admin/product/a6000.jpeg" width="370" height="320" /></div>
-					<div class="product_pultitle">상품1</div>
-					<div class="product_pulprice">23,800원</div>
-					<div class="product_pulbuycount">10,741개 구매</div>
-				</div>
-
-				<div class="product_pulinfo">
-					<div class="product_pulimg"><img src="/upload/admin/product/d750.png" width="370" height="320" /></div>
-					<div class="product_pultitle">상품1</div>
-					<div class="product_pulprice">23,800원</div>
-					<div class="product_pulbuycount">10,741개 구매</div>
-				</div>
-			</div>
-	</div>
-</section>
-
-<section id="main_sec2">
-	<div id="main_sec2_in">
-		<div id="sec1_title">마감임박</div>    
-		<div id="sec2_product">
-			<div class="product_info">
-				<div class="product_time">00:00:00</div>
-				<div class="product_title">상품1</div>
-				<div class="product_price"><span style="text-decoration : line-through;">23,800원</span><p style="margin-top:5px; font-weight:bold; color:red;">18,300원</p></div>
-				<div class="product_img"><img src="/upload/admin/product/oled.jpg" width="250" height="200" /></div>
-			</div>
-			<div class="product_info">
-				<div class="product_time">00:00:00</div>
-				<div class="product_title">상품1</div>
-				<div class="product_price">23,800원</div>
-				<div class="product_price"><span style="text-decoration : line-through;">23,800원</span><p style="margin-top:5px; font-weight:bold; color:red;">18,300원</p></div>
-				<div class="product_img"><img src="/upload/admin/product/han.jpg" width="250" height="200" /></div>
-			</div>
-		   
-	</div></div>
-</section>
-
-<section id="main_sec3">
-	<div id="main_sec3_in">
-		<div id="sec1_title">최근 본 상품</div>    
-		<div id="sec1_product">
-			<div class="product_info">
-			<div class="product_img"><img src="/upload/admin/product/s22.jpg" width="250" height="200" /></div>
-				<div class="product_title">상품1</div>
-				<div class="product_price">23,800원</div>
-			</div>
-			<div class="product_info">
-			<div class="product_img"><img src="/upload/admin/product/bike.jpg" width="250" height="200" /></div>
-				<div class="product_title">상품1</div>
-				<div class="product_price">23,800원</div>
-			</div>
-			<div class="product_info">
-			<div class="product_img"><img src="/upload/admin/product/oled.jpg" width="250" height="200" /></div>
-				<div class="product_title">상품1</div>
-				<div class="product_price">23,800원</div>
-			</div>
-			<div class="product_info">
-				<div class="product_img"><img src="/upload/admin/product/ssd.jpg" width="250" height="200" /></div>
-				<div class="product_title">상품1</div>
-				<div class="product_price">23,800원</div>
-			</div>
+        <!-- 추가부분 18.08.01 -->
+        <?php
+          $boardtime = $board['date']; //$boardtime변수에 board['date']값을 넣음
+          $timenow = date("Y-m-d"); //$timenow변수에 현재 시간 Y-M-D를 넣음
+          
+          if($boardtime==$timenow){
+            $img = "<img src='/img/new.png' alt='new' title='new' />";
+          }else{
+            $img ="";
+          }
+          ?>
+        <!-- 추가부분 18.08.01 END -->
+        <a href='/page/board/read.php?idx=<?php echo $board["idx"]; ?>'><?php echo $title; }?><span class="re_ct">[<?php echo $rep_count;?>]<?php echo $img; ?> </span></a></td>
+          <td width="120"><?php echo $board['name']?></td>
+          <td width="100"><?php echo $board['date']?></td>
+          <td width="100"><?php echo $board['hit']; ?></td>
+        </tr>
+      </tbody>
+      <?php } ?>
+    </table>
+    <div id="page_num">
+        <?php
+          if($page <= 1)
+          { //만약 page가 1보다 크거나 같다면
+            echo "<span class='fo_re'>처음</span>"; //처음이라는 글자에 빨간색 표시 
+          }else{
+            echo "<a href='?page=1'>처음</a>"; //알니라면 처음글자에 1번페이지로 갈 수있게 링크
+          }
+          if($page <= 1)
+          { //만약 page가 1보다 크거나 같다면 빈값
+            
+          }else{
+          $pre = $page-1; //pre변수에 page-1을 해준다 만약 현재 페이지가 3인데 이전버튼을 누르면 2번페이지로 갈 수 있게 함
+            echo "<a href='?page=$pre'>이전</a>"; //이전글자에 pre변수를 링크한다. 이러면 이전버튼을 누를때마다 현재 페이지에서 -1하게 된다.
+          }
+          for($i=$block_start; $i<=$block_end; $i++){ 
+            //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
+            if($page == $i){ //만약 page가 $i와 같다면 
+              echo "<span class='fo_re'>[$i]</span>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+            }else{
+              echo "<a href='?page=$i'>[$i]</a>"; //아니라면 $i
+            }
+          }
+          if($block_num >= $total_block){ //만약 현재 블록이 블록 총개수보다 크거나 같다면 빈 값
+          }else{
+            $next = $page + 1; //next변수에 page + 1을 해준다.
+            echo "<a href='?page=$next'>다음</a>"; //다음글자에 next변수를 링크한다. 현재 4페이지에 있다면 +1하여 5페이지로 이동하게 된다.
+          }
+          if($page >= $total_page){ //만약 page가 페이지수보다 크거나 같다면
+            echo "<span class='fo_re'>마지막</span>"; //마지막 글자에 긁은 빨간색을 적용한다.
+          }else{
+            echo "<a href='?page=$total_page'>마지막</a>"; //아니라면 마지막글자에 total_page를 링크한다.
+          }
+        ?>
+    </div>
+<div id="write_btn">
+      <a href="/page/board/write.php"><button>글쓰기</button></a>
 </div>
-			
-		</div>
-</section>
-</section>
+  <!-- 18.10.11 검색 추가 -->
+  <div id="search_box">
+    <form action="/page/board/search_result.php" method="get">
+      <select name="catgo">
+        <option value="title">제목</option>
+        <option value="name">글쓴이</option>
+        <option value="content">내용</option>
+      </select>
+      <input type="text" name="search" size="40" required="required" /> 
+      <button id="b_searchBtn">검색</button>
+    </form>
 
+    <form action="/index.php" method="get" id="sortForm">
+      <input type="hidden" name="cloumnName" id="cloumnName" value="" />
+      <input type="hidden" name="sortType" id="sortType" value="desc" />
+    </form>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script>
+    function b_sort(cloumn, sortType){
+      $("#cloumnName").val(cloumn);
+      switch(sortType){
+        case 'asc': sortType='desc'
+        break;
+        case 'desc': sortType='asc'
+        break;
+        default: sortType='desc'
+        break;
+      }
+      $("#sortType").val(sortType);
+      $("#sortForm").submit();
+    }
 
-<?php include $_SERVER['DOCUMENT_ROOT']."/include/footer.php"; ?>
+  </script>
+</body>
+</html>
---
+++ xampp/htdocs_쇼핑/include/header.php
@@ -0,0 +1,84 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/include/db.php"; ?>
+<!DOCTYPE html>
+<head>
+	<title>PHP 쇼핑몰</title>
+<link rel="stylesheet" href="/css/common.css" />
+<link rel="stylesheet" href="/css/sub.css" />
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+</head>
+<body>
+<header>
+	<div class="header_in">
+		<div id="logo"><a href="/">LOGO</a></div>
+		<div id="search_area">
+			<input type="text" id="search_input" name="keyword" >
+			<img src="/imgs/main_member.png" />
+		</div>
+	</div>
+	<div id="header_line"></div>
+	<div class="sec_header">
+	<div class="header_in">
+	<div id="catagory">
+		<div id="catagory_title">카테고리</div>
+		<nav id="menu">
+			<ul>
+				<li class="menu_t1"><a href="/page/browsing.php?catagory=eleTv">가전, TV</a></li>
+				<li class="menu_t2"><a href="/page/browsing.php">컴퓨터 노트북, 조립PC</a></li>
+				<li class="menu_t3"><a href="/page/browsing.php">모바일</a></li>
+				<li class="menu_t4"><a href="/page/browsing.php">카메라</a></li>
+				<li class="menu_t5"><a href="/page/browsing.php">자전거, 스포츠</a></li>
+			</ul>
+		</nav>
+	</div>
+	<img src="/upload/admin/catagory/defualt_main.png" id="defualt_main" />
+
+	<div id="catagory_hover_img">
+		<img src="/upload/admin/catagory/cat1_img.png" id="catagory_img1" />
+		<img src="/upload/admin/catagory/cat1_2img.png" id="catagory_img2" class="def_none"/>
+		<img src="/upload/admin/catagory/cat1_3img.png" id="catagory_img3" class="def_none"/>
+		<img src="/upload/admin/catagory/cat1_4img.png" id="catagory_img4" class="def_none"/>
+		<img src="/upload/admin/catagory/cat1_5img.png" id="catagory_img5" class="def_none"/>
+	</div>
+	</div>
+</header>
+
+<script>
+	$(".menu_t1").hover(function(){
+		//mouseover
+		$("#catagory_img1").show();
+	},function(){
+		//mouseout
+		$("#catagory_img1").hide();
+	});
+
+	$(".menu_t2").hover(function(){
+		$("#catagory_img2").show();
+	},function(){
+		$("#catagory_img2").hide();
+	});
+
+	$(".menu_t3").hover(function(){
+		$("#catagory_img3").show();
+	},function(){
+		$("#catagory_img3").hide();
+	});
+
+	$(".menu_t4").hover(function(){
+		//mouseover
+		$("#catagory_img4").show();
+	},function(){
+		//mouseout
+		$("#catagory_img4").hide();
+	});
+	$(".menu_t5").hover(function(){
+		//mouseover
+		$("#catagory_img5").show();
+	},function(){
+		//mouseout
+		$("#catagory_img5").hide();
+	});
+
+	
+</script>
